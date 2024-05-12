ede504889c1a433121ab6971253218baf4d6bdcb
Ekin Igdir <igdir.ekin@gmail.com>
2024-05-12 19:18:44 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@ wasm-pack build --release
 	npm run build --release
 )
 git checkout deploy
-mv ./www/dist/* .
 (git rm *.js *.wasm *.html || echo "Could not (git rm)")
+mv ./www/dist/* .
 git add *.js *.wasm *.html
 git commit -m "auto deploy"
 git push
