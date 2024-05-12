e778dad1fc9ed50ad948d505a4aaa06ef2014b13
Ekin Igdir <igdir.ekin@gmail.com>
2024-05-12 19:07:49 +0200

    deploy sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -xe
+
+wasm-pack build --release
+(
+	cd ./www
+	npm install
+	npm build --release
+)
+git checkout deploy
+mv ./www/dist/* .
+git rm *.js *.wasm *.html
+git add *.js *.wasm *.html
+git commit -m "auto deploy"
+git push
