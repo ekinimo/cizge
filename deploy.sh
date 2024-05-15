a898bc30607c925f9b791802b39c1f910ee18e56
Ekin Igdir <igdir.ekin@gmail.com>
2024-05-15 18:39:44 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,22 @@ wasm-pack build --release
 	npm run build --release
 )
 git checkout deploy
-(git rm *.js *.wasm *.html || echo "Could not (git rm)")
-git commit -m "auto delete old versions delete"
-mv ./www/dist/* .
-git add *.js *.wasm *.html
-git commit -m "auto deploy"
-git push
-git checkout dev
+(
+	(
+		git rm *.js *.wasm *.html
+		git commit -m "auto delete old versions delete"
+		mv ./www/dist/* .
+		git add *.js *.wasm *.html
+		git commit -m "auto deploy"
+		git push
+		git checkout dev
+	) ||
+		(
+			echo "Could not (git rm)"
+			mv ./www/dist/* .
+			git add *.js *.wasm *.html
+			git commit -m "auto deploy"
+			git push
+			git checkout dev
+		)
+)
