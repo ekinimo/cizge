5bdf8598a238fedeb18e0fe3ed7880090c6d5abb
Ekin Igdir <igdir.ekin@gmail.com>
2024-05-18 17:40:24 +0200

    js_funcs

--- js_funcs.js
+++ js_funcs.js
@@ -1,11 +1,27 @@
+
 export function getElemWidth(x){
+    const   hiddenDiv = document.getElementById("hidden_div");
+    if (hiddenDiv) {
+        hiddenDiv.innerHTML = x;
+        //let img_elem = document.getElementById("img");
+        
+        const foreign = document.getElementById("math");
+        const width = foreign.scrollWidth;
+        console.log("check width");
+        console.log(width);
+        foreign.remove();
+        //elem.remove();
+        //console.log("removed width");
+        return width;
+    }
     const elem = document.createElement("div");
+    elem.id="hidden_div";
     elem.style.width = "fit-content";
     elem.style.position = "absolute";
     elem.style.visibility = "hidden";
     elem.style.left = "-1000px";
     elem.style.right = "-1000px";
-
+    
     elem.innerHTML = x;
     //let img_elem = document.getElementById("img");
     document.body.appendChild(elem);
@@ -14,10 +30,21 @@ export function getElemWidth(x){
     const width = foreign.scrollWidth;
     console.log("check width");
     console.log(width);
-    elem.remove();
+    foreign.remove();
+    //elem.remove();
+    //console.log("removed width");
     return width;
 }
 export function getElemHeight(x){
+
+    const   hiddenDiv = document.getElementById("hidden_div");
+    if (hiddenDiv) {
+        hiddenDiv.innerHTML = x;
+        
+        const height = hiddenDiv.offsetHeight;
+        console.log("check width");
+        return height;
+    }
     const elem = document.createElement("div");
     elem.style.width = "fit-content";
     elem.style.position = "absolute";
@@ -27,10 +54,11 @@ export function getElemHeight(x){
 
     elem.innerHTML = x;
     document.body.appendChild(elem);
-    const width = elem.offsetHeight;
+    const height = elem.offsetHeight;
     console.log("check height");
-    console.log(width);
+    console.log(height);
     
-    elem.remove();
-    return width;
+    //elem.remove();
+    //console.log("removed height");
+    return height;
 }
